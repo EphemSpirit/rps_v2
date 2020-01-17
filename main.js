1cd68253bce2be3a74a200f67f731108c851ef59
Drew Hund <languageinvestigator@gmail.com>
2020-01-16 20:12:48 -0500

    added function to end the game at 5 wins, and then provide a button to reset the scores

--- main.js
+++ main.js
@@ -4,11 +4,26 @@ let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
 
+let newGame = document.querySelector(".reset-button");
 let playerBoard = document.querySelector("#player-score");
 let computerBoard = document.querySelector("#computer-score");
 let gameResults = document.querySelector("#game-results");
 let tieGames = document.querySelector("#tie-score");
 let buttons = document.querySelectorAll("button");
+
+let resetButton = document.createElement("button");
+resetButton.setAttribute("class", "reset-btn");
+resetButton.textContent = "New Game";
+
+resetButton.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  ties = 0;
+  playerBoard.innerHTML = "Player Score: ";
+  computerBoard.innerHTML = "Computer Score: ";
+  tieGames.innerHTML = "Ties: ";
+  gameResults.textContent = "";
+});
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -71,10 +86,23 @@ function compareChoices (playerChoice, computerChoice) {
       }
     }
     updateScores(playerScore, computerScore);
+    endGame();
   }
 
   function updateScores () {
     playerBoard.innerHTML = `Player Score: ${playerScore}`;
     computerBoard.innerHTML = `Computer Score: ${computerScore}`;
     tieGames.innerHTML = `Ties: ${ties}`;
+  }
+
+  function endGame () {
+    if (playerScore == 5) {
+      gameResults.textContent = "You won 5 games! I hope it filled the void in your life."
+      newGame.appendChild(resetButton);
+    } else if (computerScore == 5 ) {
+      gameResults.textContent = "You got beat by a machine, how does that feel."
+      newGame.appendChild(resetButton);
+    } else {
+      return;
+    }
   }
